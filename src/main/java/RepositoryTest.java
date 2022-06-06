4292667c7bf4ecbc686dc4d68c46e0504046bc47
nhjclxc <nhjclxc@163.com>
2022-06-06 14:49:53 +0800

    这是第几次提交了啊 scassa

--- src/main/java/RepositoryTest.java
+++ src/main/java/RepositoryTest.java
@@ -5,9 +5,12 @@ public class RepositoryTest {
     public static void main(String[] args) {
 
 
-        System.out.println(156156);//416
+        System.out.println(111111);//416
         System.out.println("你wwqcqw好sas");
         //compare & pull request
+        //credentials
+        //master
+        //Authorize Git Credential Manager
         //This token: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
     }
 }
