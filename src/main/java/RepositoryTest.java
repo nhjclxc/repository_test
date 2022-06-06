8273da80fd18761319e8763c9bbeeb63613abcd0
nhjclxc <nhjclxc@163.com>
2022-06-06 13:57:41 +0800

    你提交上去了吗？

---
+++ src/main/java/RepositoryTest.java
@@ -0,0 +1,12 @@
+/**
+ * @author LuoXianchao
+ * @date 2022/6/6 13:45
+ */
+public class RepositoryTest {
+    public static void main(String[] args) {
+
+
+        System.out.println(156156);//416
+        System.out.println("你wwqcqw好");
+    }
+}
